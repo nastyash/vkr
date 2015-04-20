9de5d446a91a01ca227127f7801ddb70bf08efa4
a.tolstopyatenko <anastasia.tolstopyatenko@gmail.com>
2015-04-20 12:54:40 +0300

    draw simplex

--- client/js/simplex.js
+++ client/js/simplex.js
@@ -72,6 +72,61 @@ smplxApp.controller('ShowInputFields', ['$scope', function($scope) {
           p: p,
           something: (p - 1) * ($scope.smplxDim + 1) / 2 + 1
         };
+        if ($scope.smplxDim == 2) {
+            var cnv = document.getElementById('myTriangle');
+            var ctx = cnv.getContext('2d');
+            var width = cnv.width;
+            var height = cnv.height;
+            
+            for (var x = 0.5; x < width; x += 50) {
+              ctx.moveTo(x, 0);
+              ctx.lineTo(x, height);
+            } 
+            for (var y = 0.5; y < height; y += 50) {
+              ctx.moveTo(0, y);
+              ctx.lineTo(width, y);
+            }
+            ctx.strokeStyle = "#eee";
+            ctx.stroke();
+
+            
+            ctx.beginPath();
+            ctx.moveTo(0, height - 25);
+            ctx.lineTo(width, height - 25);
+            ctx.moveTo(width - 5, height - 20);
+            ctx.lineTo(width, height - 25);
+            ctx.lineTo(width - 5, height - 30);
+
+            ctx.moveTo(50, height);
+            ctx.lineTo(50, 0);
+            ctx.moveTo(55, 5);
+            ctx.lineTo(50, 0);
+            ctx.lineTo(45, 5);
+            ctx.strokeStyle = "#000";
+            ctx.stroke(); 
+
+            function getXCoord (x) {
+                return x*200 + 50;
+            };
+            function getYCoord (y) {
+                return height - 25 - y*200;
+            };
+            ctx.beginPath();
+            ctx.moveTo(getXCoord(a[0][0]), getYCoord(a[0][1]));
+            ctx.lineTo(getXCoord(a[1][0]), getYCoord(a[1][1]));
+            ctx.lineTo(getXCoord(a[2][0]), getYCoord(a[2][1]));
+            ctx.lineTo(getXCoord(a[0][0]), getYCoord(a[0][1]));
+            ctx.strokeStyle = "#00f";
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.moveTo(getXCoord(end_points[0][0][0]), getYCoord(end_points[0][1][0]));
+            ctx.lineTo(getXCoord(end_points[1][0][0]), getYCoord(end_points[1][1][0]));
+            ctx.moveTo(getXCoord(end_points[0][0][1]), getYCoord(end_points[0][1][1]));
+            ctx.lineTo(getXCoord(end_points[1][0][1]), getYCoord(end_points[1][1][1]));
+            ctx.strokeStyle = "#0f0";
+            ctx.stroke(); 
+        }
+
         console.table(d);
         console.table(end_points);
         console.log(alpha, ksi, (p-1) * ($scope.smplxDim + 1)/2 + 1);
